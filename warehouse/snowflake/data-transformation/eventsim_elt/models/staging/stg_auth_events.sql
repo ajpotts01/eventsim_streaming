cf6a7b633a89ae618d83b1133fea2bc64e32182d
Paul Hallaste <peeterpaul.hallaste@gmail.com>
2022-12-05 15:17:53 +0900

    correct header names

--- warehouse/snowflake/data-transformation/eventsim_elt/models/staging/stg_auth_events.sql
+++ warehouse/snowflake/data-transformation/eventsim_elt/models/staging/stg_auth_events.sql
@@ -8,33 +8,35 @@ with raw_source as (
 final as (
 
     select
-        record_metadata:CreateTime::varchar as createtime,
-        record_metadata:offset::varchar as offset,
-        record_metadata:partition::varchar as partition,
-        record_metadata:topic::varchar as topic,
-        RECORD_CONTENT:SESSIONID::NUMBER as record_content_sessionid_number,
-        RECORD_CONTENT:USERID::VARCHAR as record_content_userid_string,
-        RECORD_CONTENT:TS::NUMBER as record_content_ts_number,
-        RECORD_CONTENT:STATUS::NUMBER as record_content_status_number,
-        RECORD_CONTENT:AUTH::VARCHAR as record_content_auth_string,
-        RECORD_CONTENT:ARTIST::VARCHAR as record_content_artist_string,
-        RECORD_CONTENT:CITY::VARCHAR as record_content_city_string,
-        RECORD_CONTENT:DURATION::VARCHAR as record_content_duration_string,
-        RECORD_CONTENT:FIRSTNAME::VARCHAR as record_content_firstname_string,
-        RECORD_CONTENT:LASTNAME::VARCHAR as record_content_lastname_string,
-        RECORD_CONTENT:GENDER::VARCHAR as record_content_gender_string,
-        RECORD_CONTENT:ITEMINSESSION::NUMBER as record_content_iteminsession_number,
-        RECORD_CONTENT:LEVEL::VARCHAR as record_content_level_string,
-        RECORD_CONTENT:METHOD::VARCHAR as record_content_method_string,
-        RECORD_CONTENT:PAGE::VARCHAR as record_content_page_string,
-        RECORD_CONTENT:REGISTRATION::NUMBER as record_content_registration_number,
-        RECORD_CONTENT:USERAGENT::VARCHAR as record_content_useragent_string,
-        RECORD_CONTENT:ZIP::NUMBER as record_content_zip_number,
-        RECORD_CONTENT:STATE::VARCHAR as record_content_state_string,
-        RECORD_CONTENT:LAT::NUMBER as record_content_lat_number,
-        RECORD_CONTENT:LON::NUMBER as record_content_lon_number
+        RECORD_METADATA:"CreateTime"::varchar as createtime,
+        RECORD_METADATA:"offset"::varchar as offset,
+        RECORD_METADATA:"partition"::varchar as partition,
+        RECORD_METADATA:"topic"::varchar as topic,
+        RECORD_CONTENT:"userId"::NUMBER as record_content_userid_string,
+        RECORD_CONTENT:"sessionId"::NUMBER as record_content_sessionid_number,
+        RECORD_CONTENT:"artist"::VARCHAR as record_content_artist_string,
+        RECORD_CONTENT:"ts"::NUMBER as record_content_ts_number,
+        RECORD_CONTENT:"status"::NUMBER as record_content_status_number,
+        RECORD_CONTENT:"auth"::VARCHAR as record_content_auth_string,
+        RECORD_CONTENT:"city"::VARCHAR as record_content_city_string,
+        RECORD_CONTENT:"duration"::VARCHAR as record_content_duration_string,
+        RECORD_CONTENT:"firstName"::VARCHAR as record_content_firstname_string,
+        RECORD_CONTENT:"lastName"::VARCHAR as record_content_lastname_string,
+        RECORD_CONTENT:"gender"::VARCHAR as record_content_gender_string,
+        RECORD_CONTENT:"itemInSession"::NUMBER as record_content_iteminsession_number,
+        RECORD_CONTENT:"level"::VARCHAR as record_content_level_string,
+        RECORD_CONTENT:"method"::VARCHAR as record_content_method_string,
+        RECORD_CONTENT:"page"::VARCHAR as record_content_page_string,
+        RECORD_CONTENT:"registration"::NUMBER as record_content_registration_number,
+        RECORD_CONTENT:"userAgent"::VARCHAR as record_content_useragent_string,
+        RECORD_CONTENT:"zip"::NUMBER as record_content_zip_number,
+        RECORD_CONTENT:"state"::VARCHAR as record_content_state_string,
+        RECORD_CONTENT:"lat"::NUMBER as record_content_lat_number,
+        RECORD_CONTENT:"lon"::NUMBER as record_content_lon_number
 
     from raw_source
+
+
 
 )
 
