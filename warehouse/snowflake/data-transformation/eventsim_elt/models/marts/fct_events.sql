c45675ac0b4f318993435dbd5a7af8b629f76e6b
Paul Hallaste <peeterpaul.hallaste@gmail.com>
2022-12-08 17:53:38 +0900

    add page views dim

--- warehouse/snowflake/data-transformation/eventsim_elt/models/marts/fct_events.sql
+++ warehouse/snowflake/data-transformation/eventsim_elt/models/marts/fct_events.sql
@@ -15,20 +15,18 @@ stg_status_change_events as (
 )
 
 select 
-    {{ dbt_utils.generate_surrogate_key(['auth_event_id']) }} as auth_event_key,
-    {{ dbt_utils.generate_surrogate_key(['listen_event_id']) }} as listen_event_key,
-    {{ dbt_utils.generate_surrogate_key(['page_view_id']) }} as page_view_key,
-    {{ dbt_utils.generate_surrogate_key(['status_change_event_id']) }} as status_change_event_key,
-    {{ dbt_utils.generate_surrogate_key(['session_id']) }} as session_key,
-    auth_event_id,
-    listen_event_id,
-    page_view_id,
-    status_change_event_id
+    case when stg_listen_events.topic_name = 'listen_events' then 'listen_event'
+            when stg_auth_events.topic_name = 'auth_events' then 'auth_event'
+            when stg_page_view_events.topic_name = 'page_view_events' then 'page_view_event'
+            when stg_status_change_events.topic_name = 'status_change_events' then 'status_change_event' 
+        end as event_type,
+        {{ dbt_utils.generate_surrogate_key(['stg_listen_events.artist_name']) }} as artist_key,
+        stg_page_view_events.user_key
 from
-    stg_auth_events
-full outer join stg_listen_events 
-on stg_auth_events.session_key = stg_listen_events.session_key
-full outer join stg_page_view_events
-on stg_listen_events.session_key = stg_page_view_events.session_key
-full outer join stg_status_change_events
+     stg_page_view_events
+left join stg_listen_events 
+on  stg_page_view_events.session_key = stg_listen_events.session_key
+left join stg_status_change_events
 on stg_page_view_events.session_key = stg_status_change_events.session_key
+left join stg_auth_events
+on stg_page_view_events.session_key = stg_auth_events.session_key
