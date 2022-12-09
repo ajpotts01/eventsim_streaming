984def4329ac42773e3087b98b8219e1a27ab459
Paul Hallaste <peeterpaul.hallaste@gmail.com>
2022-12-09 11:00:07 +0900

    fix formatting

--- warehouse/snowflake/data-transformation/eventsim_elt/models/marts/fct_events.sql
+++ warehouse/snowflake/data-transformation/eventsim_elt/models/marts/fct_events.sql
@@ -29,7 +29,7 @@ select
     stg_page_view_events.user_key,
 {{ dbt_utils.generate_surrogate_key(
         ['stg_page_view_events.page_view_id']) }} as page_view_key,
-        stg_page_view_events.session_key
+    stg_page_view_events.session_key
 from
     stg_page_view_events
 left join stg_listen_events
